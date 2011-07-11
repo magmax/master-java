003b085cd8cd2683128e007f9d6956b33ec61976
Miguel Ángel García <miguelangel.garcia@gmail.com>
2011-07-11 13:06:39 +0200

    práctica 1. Green. Terminada

--- practicas/practica1/src/org/magmax/masterj2ee/practica1/Main.java
+++ practicas/practica1/src/org/magmax/masterj2ee/practica1/Main.java
@@ -4,12 +4,30 @@
  */
 package org.magmax.masterj2ee.practica1;
 
+import java.io.IOException;
+
 /**
  *
  * @author Miguel Angel Garcia <miguelangel.garcia@gmail.com>
  */
 public class Main {
-    public static void main (String[] args) {
-        System.out.println("10");
+    public static void main (String[] args) throws IOException, NumeroNoValidoExcepcion {
+        Main m = new Main();
+        System.out.println("Average: " + m.average_stdin());
+    }
+    
+    private float average_stdin() throws IOException, NumeroNoValidoExcepcion {
+        Lector l = new Lector();
+        int sum = 0;
+        float nums = 0;
+        
+        while (l.ready()) {
+            sum += l.readInt();
+            nums++;
+        }
+        
+        if (nums == 0)
+            return 0;
+        return sum/nums;
     }
 }
